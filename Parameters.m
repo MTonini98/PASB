8c45fc2c11645628fa02ab73062b3b7497c6c322
Coudence <74002642+Coudence@users.noreply.github.com>
2020-11-05 14:44:08 +0100

    Update Parameters.m

--- Parameters.m
+++ Parameters.m
@@ -1,2 +1,15 @@
-parameters 
+%Parameters 
 
+%%Rat parameters
+
+QCC = 18.7 %L/hr/kg^.74
+FQliver = 0.174 %
+FQlung = 0.021
+FQbrain = 0.07
+FQfat = 0.0005
+FQgonads = 0.0079
+FQprostate = 0.021
+
+
+
+%Human parameters
