daea6df902a2dbd171122a0bbc65052296ec9d07
Coudence <74002642+Coudence@users.noreply.github.com>
2020-11-05 14:59:51 +0100

    Update Parameters.m

--- Parameters.m
+++ Parameters.m
@@ -2,14 +2,30 @@
 
 %%Rat parameters
 
-QCC = 18.7 %L/hr/kg^.74
-FQliver = 0.174 %
-FQlung = 0.021
-FQbrain = 0.07
-FQfat = 0.0005
-FQgonads = 0.0079
-FQprostate = 0.021
+QCC_rat = 18.7 %L/hr/kg^.74
+FQliver_rat = 0.174 %
+FQlung_rat = 0.021
+FQkidney_rat = 0.141
+FQbrain_rat = 0.07
+FQfat_rat = 0.0005
+FQgonads_rat = 0.0079
+FQprostate_rat = 0.021
+GE_rat = 0.37 %min-1
 
-
+Fliver_rat = 0.026
+Flung_rat = 0.0104
+Fkidney_rat = 0.0073
+Ffat_rat = 0.187
+Fgonads_rat = 0.0027
+Fplasma_rat = 0.0428
+Fprostate_rat = 0.00068
+HCT_rat = 0.46
+MSPL_rat = 38 %mg/g liver
+%Other source : MSPL_rat = 60 mg/g
+CYTPL_rat = 87 %mg/g
+PSPG_rat = 2.4 %mg/g gut
+MSPK_rat = 16 %mg/g
+CYTPK_rat = 86 %mg/g
+MSPLu_rat = 20 %mg/g lung
 
 %Human parameters
